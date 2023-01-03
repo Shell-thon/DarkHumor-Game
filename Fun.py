9f24e062c9a74ab93cfcfeaf2d0cffdba622fbfb
Shell-thon <shellterminal0@gmail.com>
2023-01-03 15:21:16 +0100

    update on fun.[y

--- Fun.py
+++ Fun.py
@@ -1,40 +1,43 @@
 #!/usr/bin/env python
 
-########################
-## AUTHOR: Adejare Michael
-########################
-print("USE LOWER CASE FOR INPUT PLEASE\n")
+print("Use lower case for input please\n")
 
-List = 1, 2, 3, 4, 5, 6, 7, 8
-cus = 9
-print("1 - python\n" + "2 - java\n" + "3 - c\n" + "4 - c#\n" + "5 - ruby\n" + "6 - perl\n" + "7 - c++\n" + "8 - php\n" + "9 - others")
-i = int(input("What language do you use: \n"))
-if i == List:
-	print("that's Cool!")
+languages = ['python', 'java', 'c', 'c#', 'ruby', 'perl', 'c++', 'php']
 
-elif i == cus:
-        customList = int(input("Enter the language you use: "))
-        print("Only geeks use " + customList)
+print("Choose a programming language:")
+for i, language in enumerate(languages, start=1):
+	print(f"{i} - {language}")
+print("9 - others")
 
-#Check for Gender
-gem = "boy"
-gemp = "girl"
-gen = input("You a boy or girl\? \n")
-
-#Girl check first
-if gen == gemp:
-	print("Hey pretty lady\:) \n")
-	dig = int(input("Enter your digit we should grab a drink sometime"))
-
-#Check for male
-elif gen == gem:
-	print("yo what\'s Up Big man \n")
-	ps5 = 1, 2, 3, 4, 5, 6
-	print("1 - COD\n" + "2 - Fifa\n" + "3 - GTA V\n" + "4 - Battlefield\n" + "5 - Apex Legends\n" + "6 - Forza Horizon\n" + "7 - No\n")
-	game = input("Which game you play? \n")
-
-#Giveaway
-if game == ps5:
-	print("mail me for giveaway shell@proton.me")
+i = int(input())
+if i < 1 or i > 9:
+	print("Invalid input")
+elif i < 9:
+	print(f"That's cool! {languages[i - 1]} is a great language!")
 else:
-	print("tf? bro you f trans? \n")
+	custom_language = input("Enter the language you use: ")
+	print(f"Only geeks use {custom_language}")
+
+# Check for gender
+gender = input("You a boy or girl? \n")
+
+# Check for girl
+if gender == 'girl':
+	print("Hey pretty lady :) \n")
+	digits = int(input("Pass in your digit shawty we should grab a drink sometime: \n"))
+	print("see ya later! \n")
+
+ # Check for male
+elif gender == 'boy':
+	print("yo what's Up Big man \n")
+	ps5 = ['cod', 'fifa', 'gta v', 'battlefield', 'apex legends', 'forza horizon']
+	print("Choose a game:")
+	for i, game in enumerate(ps5, start=1):
+		print(f"{i} - {game}")
+	print("7 - No")
+
+	game = input()
+	if game.lower() in ps5:
+		print("mail me for giveaway shell@proton.me")
+	else:
+		print("tf? bro you f trans? \n")
